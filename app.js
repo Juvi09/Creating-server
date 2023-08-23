a48550af5a080235b69f63bf24fdcbe90899c477
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-08-23 04:15:44 +0200

    server listen func & error func.

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const server = http.createServer(function(req, res){
 
 });
 
-server.listen(port)
+server.listen(port, function(error){
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port ' + port);
+    }
+})
 
